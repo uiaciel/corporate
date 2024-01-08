7e4164130398092e7fe7abae78a6895f3fff98e6
uiaciel <uiaciel@gmail.com>
2024-01-08 20:00:31 +0700

    fix: login admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,159 @@
 @extends('layouts.app')
 
 @section('content')
-<main class="d-flex w-100 h-100">
-		<div class="container d-flex flex-column">
-			<div class="row vh-100">
-				<div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
-					<div class="d-table-cell align-middle">
+<style>body {
+    font-family: "Roboto", sans-serif;
+    background-color: #fff; }
 
-						<div class="text-center mt-4">
-							<h1 class="h2">Admin Login</h1>
-							<p class="lead">
-								{{ $setting->sitename }}
-							</p>
-						</div>
+  p {
+    color: #b3b3b3;
+    font-weight: 300; }
 
-						<div class="card">
-							<div class="card-body">
-								<div class="m-sm-3">
-                                    @if ($errors->any())
+  h1, h2, h3, h4, h5, h6,
+  .h1, .h2, .h3, .h4, .h5, .h6 {
+    font-family: "Roboto", sans-serif; }
+
+  a {
+    -webkit-transition: .3s all ease;
+    -o-transition: .3s all ease;
+    transition: .3s all ease; }
+    a:hover {
+      text-decoration: none !important; }
+
+  .content {
+    padding: 7rem 0; }
+
+  h2 {
+    font-size: 20px; }
+
+  .half, .half .container > .row {
+    height: 100vh;
+    min-height: 700px; }
+
+  @media (max-width: 991.98px) {
+    .half .bg {
+      height: 200px; } }
+
+  .half .contents {
+    background: #f6f7fc; }
+
+  .half .contents, .half .bg {
+    width: 50%; }
+    @media (max-width: 1199.98px) {
+      .half .contents, .half .bg {
+        width: 100%; } }
+    .half .contents .form-control, .half .bg .form-control {
+      border: none;
+      -webkit-box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
+      box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
+      border-radius: 4px;
+      height: 54px;
+      background: #fff; }
+      .half .contents .form-control:active, .half .contents .form-control:focus, .half .bg .form-control:active, .half .bg .form-control:focus {
+        outline: none;
+        -webkit-box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
+        box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1); }
+
+  .half .bg {
+    background-size: cover;
+    background-position: center; }
+
+  .half a {
+    color: #888;
+    text-decoration: underline; }
+
+  .half .btn {
+    height: 54px;
+    padding-left: 30px;
+    padding-right: 30px; }
+
+  .half .forgot-pass {
+    position: relative;
+    top: 2px;
+    font-size: 14px; }
+
+  .control {
+    display: block;
+    position: relative;
+    padding-left: 30px;
+    margin-bottom: 15px;
+    cursor: pointer;
+    font-size: 14px; }
+    .control .caption {
+      position: relative;
+      top: .2rem;
+      color: #888; }
+
+  .control input {
+    position: absolute;
+    z-index: -1;
+    opacity: 0; }
+
+  .control__indicator {
+    position: absolute;
+    top: 2px;
+    left: 0;
+    height: 20px;
+    width: 20px;
+    background: #e6e6e6;
+    border-radius: 4px; }
+
+  .control--radio .control__indicator {
+    border-radius: 50%; }
+
+  .control:hover input ~ .control__indicator,
+  .control input:focus ~ .control__indicator {
+    background: #ccc; }
+
+  .control input:checked ~ .control__indicator {
+    background: #fb771a; }
+
+  .control:hover input:not([disabled]):checked ~ .control__indicator,
+  .control input:checked:focus ~ .control__indicator {
+    background: #fb8633; }
+
+  .control input:disabled ~ .control__indicator {
+    background: #e6e6e6;
+    opacity: 0.9;
+    pointer-events: none; }
+
+  .control__indicator:after {
+    font-family: 'icomoon';
+    content: '\e5ca';
+    position: absolute;
+    display: none;
+    font-size: 16px;
+    -webkit-transition: .3s all ease;
+    -o-transition: .3s all ease;
+    transition: .3s all ease; }
+
+  .control input:checked ~ .control__indicator:after {
+    display: block;
+    color: #fff; }
+
+  .control--checkbox .control__indicator:after {
+    top: 50%;
+    left: 50%;
+    margin-top: -1px;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%); }
+
+  .control--checkbox input:disabled ~ .control__indicator:after {
+    border-color: #7b7b7b; }
+
+  .control--checkbox input:disabled:checked ~ .control__indicator {
+    background-color: #7e0cf5;
+    opacity: .2; }</style>
+<div class="d-lg-flex half">
+    <div class="bg order-1 order-md-1"
+        style="background-image: url('https://preview.colorlib.com/theme/bootstrap/login-form-02/images/bg_1.jpg.webp');">
+    </div>
+    <div class="contents order-2 order-md-2">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-md-7">
+                    @if ($errors->any())
                                         <div class="alert alert-danger">
                                             <ul>
                                                 @foreach ($errors->all() as $error)
@@ -27,39 +163,32 @@
                                         </div>
                                     @endif
 
-                                    <form method="POST" action="{{ route('login') }}">
-                                        @csrf
-										<div class="mb-3">
-											<label class="form-label">Email</label>
-											<input class="form-control form-control-lg" type="email" name="email" placeholder="Enter your email" required>
-										</div>
-										<div class="mb-3">
-											<label class="form-label">Password</label>
-											<input class="form-control form-control-lg" type="password" name="password" placeholder="Enter your password" required>
-											{{-- <small>
-												<a href="/pages-reset-password">Forgot password?</a>
-											</small> --}}
-										</div>
-										<div>
-											<div class="form-check align-items-center">
-												<input id="customControlInline" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-												<label class="form-check-label text-small" for="customControlInline">Remember me</label>
-											</div>
-										</div>
-										<div class="d-grid gap-2 mt-3">
-                                            <button type="submit" class="btn btn-primary">Sign In</button>
-										</div>
-									</form>
-								</div>
-							</div>
-						</div>
-						<div class="text-center mt-3">
-							<a href="/">Contact Support</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</main>
+                    <h3>Login to <strong>{{ $setting->sitename }}</strong></h3>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group first mb-3">
+
+                            <input class="form-control" type="email" name="email" placeholder="Enter your email" required>
+                        </div>
+                        <div class="form-group last mb-3">
+
+                            <input class="form-control" type="password" name="password" placeholder="Enter your password" required>
+
+                        </div>
+                        <div class="d-flex mb-5 align-items-center">
+                            <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
+                                <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <div class="control__indicator"></div>
+                            </label>
+                            {{-- <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span> --}}
+                        </div>
+                        <input type="submit" value="Log In" class="btn btn-primary">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 </head>
 <body>
     <div id="app">
-        <main class="py-4">
+        <main class="">
             @yield('content')
         </main>
     </div>
